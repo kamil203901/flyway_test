33aa3ceacdbf94cfb14bdc43699738374c9fb27e
Kamil <kazmierczak.kamil1996@gmail.com>
2019-10-20 21:44:33 +0200

    First commit for tests with flyway on Oracle

---
+++ src/main/java/pl/spring/oracle/demo/config/DatabaseConfiguration.java
@@ -0,0 +1,34 @@
+package pl.spring.oracle.demo.config;
+
+import oracle.jdbc.pool.OracleDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${oracle.username}")
+    private String username;
+
+    @Value("${oracle.password}")
+    private String password;
+
+    @Value("${oracle.url}")
+    private String url;
+
+    @Bean
+    DataSource dataSource() throws SQLException {
+        OracleDataSource dataSource = new OracleDataSource();
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        dataSource.setURL(url);
+        dataSource.setImplicitCachingEnabled(true);
+        dataSource.setFastConnectionFailoverEnabled(true);
+        return dataSource;
+    }
+
+}
